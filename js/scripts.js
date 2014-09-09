84b8b3566393bd72f40dc9b29d3d9f0b9d800b35
brettex <brettex@hotmail.com>
2014-09-09 13:36:53 -0700

    Login Portal

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,38 @@ jQuery(function() {
 
 jQuery(document).ready( function($){
 	
+		
+	/* When using absolutely positioned labels for the Form elements
+	   we need to hide/show the labels during focus/blur events
+	   The parent Form must have the .showLabels class
+	 */
+	$('.showLabels input').each( function (){
+		//Define the label elmenent
+		var label = $(this).parent('div').find('label');
+
+		// If already filled in on Page Load, hide label
+		if($(this).val() != ''){
+			label.animate({'opacity': 0.0}, 300);
+		}
+		//On Focus, hide the label
+		$(this).focus( function(){
+			label.animate({'opacity': 0.0}, 300);
+		});
+		// Show Label if Value of Field is still empty
+		$(this).blur( function(){
+				if($(this).val() == ""){
+					label.show();
+					label.animate({'opacity': 1.0}, 300);
+				}
+				else{
+					label.hide();
+				}
+					
+		});
+		$(this).keypress( function(){
+			label.hide();
+		});
+	
+	}); 
+	
 });
